46d1e5513a226d29caa2833b19f839413e92e628
Sugene Mulpuri <sugenemulpuri9@gmail.com>
2024-01-31 16:09:11 -0600

    phase 0 intro to js 2 array lab

--- index.js
+++ index.js
@@ -1,1 +1,63 @@
 // Write your solution here!
+
+const cats = ["Milo", "Otis", "Garfield"]
+
+beforeEach(function () {
+    cats.length = 0;
+  
+    cats.push("Milo", "Otis", "Garfield");
+  });
+
+
+function destructivelyAppendCat(name) {
+cats.push("Ralph")
+return cats;
+};
+
+function destructivelyPrependCat(name) {
+
+cats.unshift("Bob");
+return cats;
+
+};
+
+function destructivelyRemoveLastCat() {
+cats.pop();
+return cats;
+}
+
+function destructivelyRemoveFirstCat() {
+
+cats.shift();
+return cats;
+
+}
+
+function appendCat(name) {
+const copyOfCats = [...cats];
+
+copyOfCats.push("Broom");
+return copyOfCats;
+
+}
+
+function prependCat(name) {
+const copyOfCats = [...cats];
+copyOfCats.unshift("Arnold");
+return copyOfCats;
+
+}
+
+function removeLastCat() {
+const copyOfCats = [...cats];
+copyOfCats.pop();
+return copyOfCats;
+
+}
+
+function removeFirstCat() {
+const copyOfCats = [...cats];
+copyOfCats.shift();
+return copyOfCats;
+
+}
